93354071659bf590a8a62aa7ed46f3eba457510f
Fernando Tzoc <fernandotzoc4@gmail.com>
2021-11-24 23:49:19 -0600

    Component extend styles ready

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,24 +1,34 @@
-import {useContext} from "react";
-import {ProductContext} from "./ProductCard";
+import { CSSProperties, useContext } from "react";
+import { ProductContext } from "./ProductCard";
 
 import noImage from "../assets/no-image.jpg";
 import styles from "../styles/styles.module.css";
 
-export const ProductImage = ({img = ''}) => {
+export interface Props {
+  img?: string;
+  className?: string;
+  style?: CSSProperties;
+}
 
-  const { product } = useContext( ProductContext );
+export const ProductImage = ({ img, className, style }: Props) => {
+  const { product } = useContext(ProductContext);
 
   let imgToShow: string;
 
-  if( img ) {
+  if (img) {
     imgToShow = img;
-  } else if ( product.img ) {
-    imgToShow = product.img
+  } else if (product.img) {
+    imgToShow = product.img;
   } else {
     imgToShow = noImage;
   }
 
   return (
-    <img className={ styles.productImg } src={ imgToShow  } alt="Product Image"  />
-  )
-}
+    <img
+      className={`${styles.productImg} ${className}`}
+      src={imgToShow}
+      alt="Product Image"
+      style={ style }
+    />
+  );
+};
